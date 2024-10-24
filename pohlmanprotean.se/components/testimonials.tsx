626882596da90dfec8d3d78c7a561a2b58fefd1d
Robin Pohlman <robin.pohlman@gmail.com>
2024-10-24 09:59:27 +0200

    fix APP-0004: fix naming

---
+++ pohlmanprotean.se/components/testimonials.tsx
@@ -0,0 +1,237 @@
+"use client";
+
+import { useState } from "react";
+import useMasonry from "@/utils/useMasonry";
+import Image, { StaticImageData } from "next/image";
+import docker from "@/public/images/docker.svg";
+import bash from "@/public/images/bash.svg";
+import javascript from "@/public/images/javascript.svg";
+import typescript from "@/public/images/typescript.svg";
+import github from "@/public/images/github.svg";
+import googlecloud from "@/public/images/googlecloud.svg";
+import ansible from "@/public/images/ansible.svg";
+import nodejs from "@/public/images/nodejs.svg";
+import prisma from "@/public/images/prisma.svg";
+
+
+const testimonials = [
+  {
+    clientImg: bash,
+    name: "Bash",
+    company: "",
+    content:
+    "Bash (Bourne Again SHell) is a Unix-based command-line interpreter and scripting language commonly used in Linux and macOS environments. It allows users to interact with the operating system by executing commands, automating tasks, and running scripts.",
+    categories: [1, 2, 5],
+  },
+  {
+    clientImg: javascript,
+    name: "Andrew K.",
+    company: "Samsung",
+    content:
+    "JavaScript is a high-level, dynamic programming language primarily used to create interactive and dynamic content on websites. It runs in web browsers and enables developers to build features like animations, form validations, real-time updates, and user interface elements.",
+    categories: [1, 2,],
+  },
+  {
+    clientImg: typescript,
+    name: "Lucy D.",
+    company: "Rio",
+    content:
+    "TypeScript is a superset of JavaScript that adds static typing, allowing developers to define types for variables, function parameters, and return values. ",
+    categories: [1, 2, 5],
+  },
+  {
+    clientImg: docker,
+    name: "Pavel M.",
+    company: "Canon",
+    content: "Docker is a platform that enables developers to automate the deployment of applications inside lightweight, portable containers. Containers package the application code, dependencies, and runtime environment, ensuring the application runs consistently across different systems.",
+    categories: [1, 3],
+  },
+  {
+    clientImg: github,
+    name: "Miriam E.",
+    company: "Cadbury",
+    content: "GitHub is a web-based platform for version control and collaboration, primarily used by developers to host and manage code. It leverages Git, a version control system, to track changes in projects, enabling multiple contributors to work on the same codebase simultaneously.",
+    categories: [1, 3, 4],
+  },
+  {
+    clientImg: googlecloud,
+    name: "Eloise V.",
+    company: "Maffell",
+    content:
+    "Google Cloud Run is a fully managed platform that allows developers to run containerized applications in the cloud without worrying about infrastructure management.",
+    categories: [1, 3, 4],
+  },
+  {
+    clientImg: ansible,
+    name: "Pierre-Gilles L.",
+    company: "Binance",
+    content:
+   "Ansible is an open-source automation tool used for configuration management, application deployment, and task automation. It allows developers and system administrators to automate repetitive tasks across multiple systems by writing simple, human-readable YAML files called playbooks.", 
+   categories: [1, 3, 4],
+  },
+  {
+    clientImg: nodejs,
+    name: "Danielle K.",
+    company: "Forbes Inc.",
+    content:
+    "Node.js is an open-source, server-side runtime environment that allows developers to run JavaScript outside the browser. Built on the V8 JavaScript engine, it enables the development of scalable, high-performance applications, particularly for handling asynchronous, I/O-intensive tasks like web servers and APIs. ",
+    categories: [1, 3],
+  },
+  {
+    clientImg: prisma,
+    name: "Mary P.",
+    company: "Ray Ban",
+    content:
+    "Prisma is an open-source ORM (Object-Relational Mapping) tool that simplifies database access and management for developers. It provides a type-safe interface for interacting with databases, ensuring that queries are checked at compile time, which helps catch errors early.",
+    categories: [1, 3],
+  },
+];
+
+export default function Testimonials() {
+  const masonryContainer = useMasonry();
+  const [category, setCategory] = useState<number>(1);
+
+  return (
+    <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="border-t py-12 [border-image:linear-gradient(to_right,transparent,theme(colors.slate.400/.25),transparent)1] md:py-20">
+        {/* Section header */}
+        <div className="mx-auto max-w-3xl pb-12 text-center">
+          <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
+          Technologies We Use
+          </h2>
+          <p className="text-lg text-indigo-200/65">
+          We use the following technologies for our services:
+          </p>
+        </div>
+
+        <div>
+          {/* Buttons */}
+          <div className="flex justify-center pb-12 max-md:hidden md:pb-16">
+            <div className="relative inline-flex flex-wrap justify-center rounded-[1.25rem] bg-gray-800/40 p-1">
+              {/* Button #1 */}
+              <button
+                className={`flex h-8 flex-1 items-center gap-2.5 whitespace-nowrap rounded-full px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-200 ${category === 1 ? "relative bg-gradient-to-b from-gray-900 via-gray-800/60 to-gray-900 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.indigo.500/0),theme(colors.indigo.500/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]" : "opacity-65 transition-opacity hover:opacity-90"}`}
+                aria-pressed={category === 1}
+                onClick={() => setCategory(1)}
+              >
+                <svg
+                  className={`fill-current ${category === 1 ? "text-indigo-500" : "text-gray-600"}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height={16}
+                >
+                  <path d="M.062 10.003a1 1 0 0 1 1.947.455c-.019.08.01.152.078.19l5.83 3.333c.052.03.115.03.168 0l5.83-3.333a.163.163 0 0 0 .078-.188 1 1 0 0 1 1.947-.459 2.161 2.161 0 0 1-1.032 2.384l-5.83 3.331a2.168 2.168 0 0 1-2.154 0l-5.83-3.331a2.162 2.162 0 0 1-1.032-2.382Zm7.854-7.981-5.83 3.332a.17.17 0 0 0 0 .295l5.828 3.33c.054.031.118.031.17.002l5.83-3.333a.17.17 0 0 0 0-.294L8.085 2.023a.172.172 0 0 0-.17-.001ZM9.076.285l5.83 3.332c1.458.833 1.458 2.935 0 3.768l-5.83 3.333c-.667.38-1.485.38-2.153-.001l-5.83-3.332c-1.457-.833-1.457-2.935 0-3.767L6.925.285a2.173 2.173 0 0 1 2.15 0Z" />
+                </svg>
+                <span>View All</span>
+              </button>
+              {/* Button #2 */}
+              <button
+                className={`flex h-8 flex-1 items-center gap-2.5 whitespace-nowrap rounded-full px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-200 ${category === 2 ? "relative bg-gradient-to-b from-gray-900 via-gray-800/60 to-gray-900 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.indigo.500/0),theme(colors.indigo.500/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]" : "opacity-65 transition-opacity hover:opacity-90"}`}
+                aria-pressed={category === 2}
+                onClick={() => setCategory(2)}
+              >
+                <svg
+                  className={`fill-current ${category === 2 ? "text-indigo-500" : "text-gray-600"}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height={16}
+                >
+                  <path d="M6.5 3.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM9 6.855A3.502 3.502 0 0 0 8 0a3.5 3.5 0 0 0-1 6.855v1.656L5.534 9.65a3.5 3.5 0 1 0 1.229 1.578L8 10.267l1.238.962a3.5 3.5 0 1 0 1.229-1.578L9 8.511V6.855Zm2.303 4.74c.005-.005.01-.01.013-.016l.012-.016a1.5 1.5 0 1 1-.025.032ZM3.5 11A1.497 1.497 0 0 1 5 12.5 1.5 1.5 0 1 1 3.5 11Z" />
+                </svg>
+                <span>Languages</span>
+              </button>
+              {/* Button #3 */}
+              <button
+                className={`flex h-8 flex-1 items-center gap-2.5 whitespace-nowrap rounded-full px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-200 ${category === 3 ? "relative bg-gradient-to-b from-gray-900 via-gray-800/60 to-gray-900 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.indigo.500/0),theme(colors.indigo.500/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]" : "opacity-65 transition-opacity hover:opacity-90"}`}
+                aria-pressed={category === 3}
+                onClick={() => setCategory(3)}
+              >
+                <svg
+                  className={`fill-current ${category === 3 ? "text-indigo-500" : "text-gray-600"}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height={16}
+                >
+                  <path d="M2.428 10c.665-1.815 1.98-3.604 3.44-4.802-.6-1.807-1.443-3.079-2.29-3.18-1.91-.227-2.246 2.04-.174 2.962a1 1 0 1 1-.813 1.827C-1.407 5.028-.589-.491 3.815.032c1.605.191 2.925 1.811 3.79 4.07.979-.427 1.937-.51 2.735-.092.818.429 1.143 1.123 1.294 2.148.015.1.022.149.043.32.542-.537 1.003-.797 1.693-.622.64.162.894.493 1.195 1.147l.018.04a1 1 0 0 1 1.133 1.61c-.46.47-1.12.574-1.744.398a1.661 1.661 0 0 1-.87-.592 2.127 2.127 0 0 1-.224-.349 3.225 3.225 0 0 1-.55.477c-.377.253-.8.368-1.259.267-.993-.218-1.21-.779-1.367-2.05-.027-.22-.033-.262-.046-.353-.067-.452-.144-.617-.244-.67-.225-.118-.665-.013-1.206.278.297 1.243.475 2.587.516 3.941H15a1 1 0 0 1 0 2H8.68l-.025.285c-.173 1.918-.906 3.381-2.654 3.668-1.5.246-3.013-.47-3.677-1.858-.29-.637-.39-1.35-.342-2.095H1a1 1 0 0 1 0-2h1.428Zm2.11 0h2.175a18.602 18.602 0 0 0-.284-2.577c-.205.202-.408.42-.606.654A9.596 9.596 0 0 0 4.537 10Zm2.135 2H3.942c-.032.465.03.888.194 1.25.258.538.89.836 1.54.73.546-.09.888-.772.988-1.875L6.673 12Z" />
+                </svg>
+                <span>Dev tools</span>
+              </button>
+              {/* Button #4 */}
+              <button
+                className={`flex h-8 flex-1 items-center gap-2.5 whitespace-nowrap rounded-full px-3 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-200 ${category === 4 ? "relative bg-gradient-to-b from-gray-900 via-gray-800/60 to-gray-900 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.indigo.500/0),theme(colors.indigo.500/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]" : "opacity-65 transition-opacity hover:opacity-90"}`}
+                aria-pressed={category === 4}
+                onClick={() => setCategory(4)}
+              >
+                <svg
+                  className={`fill-current ${category === 4 ? "text-indigo-500" : "text-gray-600"}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height={16}
+                >
+                  <path d="M3.757 3.758a6 6 0 0 1 8.485 8.485 5.992 5.992 0 0 1-5.301 1.664 1 1 0 1 0-.351 1.969 8 8 0 1 0-4.247-2.218 1 1 0 0 0 1.415-.001L9.12 8.294v1.827a1 1 0 1 0 2 0v-4.2a.997.997 0 0 0-1-1.042H5.879a1 1 0 1 0 0 2h1.829l-4.599 4.598a6 6 0 0 1 .648-7.719Z" />
+                </svg>
+                <span>Cloud</span>
+              </button>
+            </div>
+          </div>
+          {/* Cards */}
+          <div
+            className="mx-auto grid max-w-sm items-start gap-6 sm:max-w-none sm:grid-cols-2 lg:grid-cols-3"
+            ref={masonryContainer}
+          >
+            {testimonials.map((testimonial, index) => (
+              <div key={index} className="group">
+                <Testimonial testimonial={testimonial} category={category}>
+                  {testimonial.content}
+                </Testimonial>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Testimonial({
+  testimonial,
+  category,
+  children,
+}: {
+  testimonial: {
+    clientImg: StaticImageData;
+    name: string;
+    company: string;
+    content: string;
+    categories: number[];
+  };
+  category: number;
+  children: React.ReactNode;
+}) {
+  return (
+    <article
+      className={`relative rounded-2xl bg-gradient-to-br from-gray-900/50 via-gray-800/25 to-gray-900/50 p-5 backdrop-blur-sm transition-opacity before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] ${!testimonial.categories.includes(category) ? "opacity-30" : ""}`}
+    >
+      <div className="flex flex-col gap-4">
+        <div>
+          <Image src={testimonial.clientImg} height={36} alt="Client logo" />
+        </div>
+        <p className="text-indigo-200/65 before:content-['“'] after:content-['”']">
+          {children}
+        </p>
+        <div className="flex items-center gap-3">
+          <div className="text-sm font-medium text-gray-200">
+            <span>{testimonial.name}</span>
+            <span className="text-gray-700"> </span>
+            <a
+              className="text-indigo-200/65 transition-colors hover:text-indigo-500"
+              href="#0"
+            >
+              {testimonial.company}
+            </a>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
