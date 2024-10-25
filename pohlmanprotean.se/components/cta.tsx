04bc82f31b7317ead3c4c930a97e0b744a7a757f
Robin Pohlman <robin.pohlman@gmail.com>
2024-10-25 08:48:07 +0200

    Feat APP-0004: Update contact us section

--- pohlmanprotean.se/components/cta.tsx
+++ pohlmanprotean.se/components/cta.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import BlurredShape from "@/public/images/blurred-shape.svg";
+import Spotlight from "@/components/spotlight";
 
 export default function Cta() {
   return (
@@ -21,31 +22,77 @@ export default function Cta() {
           <div className="mx-auto max-w-3xl text-center">
             <h2
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-8 font-nacelle text-3xl font-semibold text-transparent md:text-4xl"
-              data-aos="fade-up"
-            >
-              Contact us for more info
+              >
+                Contact us for more info           
             </h2>
+          {/* Spotlight items */}
+          <Spotlight className="group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-3 flex justify-center lg:grid-cols-1"
+          data-aos="fade-up">
+  {/* Card 1 */}
+  <a
+    className="group/card relative h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100"
+    href="#0">
+    <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-gradient-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
+      {/* Content */}
+      <div className="p-6">
+  <div className="mb-3 flex gap-4">
+    {/* First Name */}
+    
+    <input
+      type="text"
+      placeholder="First Name"
+      className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-sm font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+    />
+    {/* Last Name */}
+    <input
+      type="text"
+      placeholder="Last Name"
+      className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-sm font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+    />
+  </div>
+        {/* Other Fields */}
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Company"
+            className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-xs font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Job Title"
+            className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-xs font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Email"
+            className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-xs font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-xs font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Message"
+            className="btn-sm w-full rounded-full bg-gray-800/40 px-4 py-4 text-xs font-normal text-indigo-200 placeholder-indigo-200/60 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          />
+        </div>
+      </div>
+    </div>
+  </a>
+</Spotlight>
+
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <div data-aos="fade-up" data-aos-delay={400}>
-                <a
-                  className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                  href="#0"
-                >
-                  <span className="relative inline-flex items-center">
-                    Contact Us
-                    <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
-                      -&gt;
-                    </span>
-                  </span>
-                </a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay={600}>
-                <a
-                  className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                  href="#0"
-                >
-                  Schedule Demo
-                </a>
               </div>
             </div>
           </div>
